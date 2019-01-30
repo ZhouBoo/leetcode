d66be8cf7ed6bb3b36aff117764ea14f7ed8d0eb
Bob Zhou <zhoubo392@gmail.com>
2019-01-30 15:16:42 +0800

    [update] -> 小米的算法题库目录添加

--- python/926.py
+++ python/926.py
@@ -9,14 +9,48 @@ class Solution:
         :type S: str
         :rtype: int
         """
+        fix_string = '%d' % int(S)
+        print('begin ===> %s\n' % fix_string)
+        last_number = None
+        last_count = None
+        change_count = 0
+        temp_count = 0
+        change_count = 0
+        length = len(fix_string)
+        jump_next = False
+
+        for (idx, s) in enumerate(fix_string):
+            temp_count += 1
+            if not last_count:
+                last_count = 0
+
+            if idx + 1 >= length or s != fix_string[idx + 1]:
+                if not jump_next:
+                    if last_count > temp_count:
+                        print('++ current -- last count = %d, current count = %d' % (last_count, temp_count))
+                        change_count += temp_count
+                        jump_next = True
+
+                    if temp_count > last_count:
+                        print('++ last -- last count = %d, current count = %d' % (last_count, temp_count))
+                        change_count += last_count
+
+                else:
+                    jump_next = False
+
+                last_count = temp_count
+                temp_count = 0
+                last_number = s
+
+        return change_count
 
 
-        
-
-# print(Solution().minFlipsMonoIncr("010110") == 2)
-# print(Solution().minFlipsMonoIncr("00110") == 1)
-# print(Solution().minFlipsMonoIncr("00011000") == 2)
-# print(Solution().minFlipsMonoIncr("11111") == 0)
-# print(Solution().minFlipsMonoIncr("0101100011") == 3)
-# print(Solution().minFlipsMonoIncr("011110110111111") == 2)
+print(Solution().minFlipsMonoIncr("010110") == 2)
+print(Solution().minFlipsMonoIncr("00110") == 1)
+print(Solution().minFlipsMonoIncr("0011000") == 2)
+print(Solution().minFlipsMonoIncr("11111") == 0)
+print(Solution().minFlipsMonoIncr("0101100011") == 3)
+print(Solution().minFlipsMonoIncr("011110110111111") == 2)
 print(Solution().minFlipsMonoIncr("10011111110010111011") == 5)
+# +1 -2 +7 -2 +1 -1 +3 -1 +2
+# 1  -1 +6 +4 +5 +4 +7 +6 +8
