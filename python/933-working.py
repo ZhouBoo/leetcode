f6b28cbd9b1f0db1106e622759d47b2ce11ce9ff
Bob Zhou <zhoubo>
2020-01-06 20:21:16 +0800

    update: 48 medium

--- python/933-working.py
+++ python/933-working.py
@@ -8,17 +8,36 @@
 # 输出：[null,1,2,3,3]
 
 
-class RecentCounter:
-    def __init__(self):
-        pass
+# class RecentCounter:
+#     def __init__(self):
+#         pass
     
-    def ping(self, t):
-        """
-        :type t: int
-        :rtype: int
-        """
+#     def ping(self, t):
+#         """
+#         :type t: int
+#         :rtype: int
+#         """
 
 
 # Your RecentCounter object will be instantiated and called as such:
 # obj = RecentCounter()
 # param_1 = obj.ping(t)
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+# 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+# 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和
+# 中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+class Solution:
+    def reConstructBinaryTree(self, pre, tin):
+        # write code here
+        if len(pre) <= 0:
+            return None
+        for index in pre:
+            
